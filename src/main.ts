ef5973d3075c9d98f36ad2e9ce9e6115e383a287
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-09-30 16:14:24 +0100

    Release v0.1.0
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { getRemoteMetdataTable } from "./ui.ts";
 await new Command()
   .name("denotag")
   .description("OGG Opus music tagger")
-  .version("0.0.0")
+  .version("0.1.0")
   .option(
     "-f, --file <filePath:string>",
     "The ogg file",
