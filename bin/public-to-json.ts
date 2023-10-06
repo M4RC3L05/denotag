39679313eadfabdfc2ffa3fde9e47b7e1c912772
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-10-06 12:40:31 +0100

    Handle new files on embed
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- bin/public-to-json.ts
+++ bin/public-to-json.ts
@@ -1,17 +1,29 @@
 #!/usr/bin/env -S deno run -A --unstable
 
-import { walk } from "../src/deps.ts";
+import { extname, transpile, walk } from "../src/deps.ts";
+import json from "../deno.json" assert { type: "json" };
 
 const data: Record<string, number[]> = {};
 const dirWalker = walk("./public", {
-  exts: ["js", "html"],
+  exts: ["ts", "tsx", ".css", "html"],
   includeDirs: false,
   includeSymlinks: false,
 });
 
 for await (const file of dirWalker) {
-  const contents = await Deno.readFile(file.path);
-  data[file.path] = Array.from(contents);
+  if ([".ts", ".tsx"].includes(extname(file.path))) {
+    const compiled = await transpile(file.path, {
+      importMap: { imports: json.imports },
+    });
+
+    for (const [k, v] of compiled.entries()) {
+      if (!(k in data)) data[k] = Array.from(new TextEncoder().encode(v));
+    }
+
+    continue;
+  }
+
+  data[file.path] = Array.from(await Deno.readFile(file.path));
 }
 
 await Deno.writeTextFile("./src/public.json", JSON.stringify(data));
