e6a5ceb0799fb2cebbb422fe8ee351902e60ecd2
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-11-17 19:58:40 +0000

    Update dependencies
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/deps.ts
+++ src/deps.ts
@@ -8,12 +8,12 @@ export {
   File,
   OggTag,
   PictureType,
-} from "npm:node-taglib-sharp@5.1.0";
-export { fileTypeFromBuffer } from "npm:file-type@18.5.0";
+} from "npm:node-taglib-sharp@5.2.1";
+export { fileTypeFromBuffer } from "npm:file-type@18.7.0";
 export { Webview } from "https://deno.land/x/webview@0.7.6/mod.ts";
-export { decodeBase64 } from "https://deno.land/std@0.203.0/encoding/base64.ts";
-export { serveDir } from "https://deno.land/std@0.203.0/http/file_server.ts";
-export { extname, SEP } from "https://deno.land/std@0.203.0/path/mod.ts";
-export { walk } from "https://deno.land/std@0.203.0/fs/mod.ts";
-export { transpile } from "https://deno.land/x/emit@0.29.0/mod.ts";
+export { decodeBase64 } from "https://deno.land/std@0.207.0/encoding/base64.ts";
+export { serveDir } from "https://deno.land/std@0.207.0/http/file_server.ts";
+export { extname, SEP } from "https://deno.land/std@0.207.0/path/mod.ts";
+export { walk } from "https://deno.land/std@0.207.0/fs/mod.ts";
+export { transpile } from "https://deno.land/x/emit@0.31.4/mod.ts";
 export { z as zod } from "https://deno.land/x/zod@v3.22.4/mod.ts";
