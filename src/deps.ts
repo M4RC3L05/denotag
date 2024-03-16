5c9fc87bd4747d8ace14cfc126b060d81a5eaee5
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-03-16 19:28:35 +0000

    Update dependencies
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/deps.ts
+++ src/deps.ts
@@ -9,8 +9,8 @@ export {
   OggTag,
   PictureType,
 } from "npm:node-taglib-sharp@5.2.3";
-export { SizeHint, Webview } from "https://deno.land/x/webview@0.7.6/mod.ts";
-export { decodeBase64 } from "https://deno.land/std@0.216.0/encoding/base64.ts";
-export { join } from "https://deno.land/std@0.216.0/path/mod.ts";
-export { build, type Plugin } from "https://deno.land/x/esbuild@v0.20.0/mod.js";
+export { SizeHint, Webview } from "jsr:@webview/webview@^0.8.0";
+export { decodeBase64 } from "https://deno.land/std@0.220.1/encoding/base64.ts";
+export { join } from "https://deno.land/std@0.220.1/path/mod.ts";
+export { build, type Plugin } from "https://deno.land/x/esbuild@v0.20.2/mod.js";
 export { cache as esbuildPluginCache } from "npm:esbuild-plugin-cache@0.2.10";
