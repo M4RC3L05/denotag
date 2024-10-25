1499618d1f3d9ad2f2eb24eadf4eec0f19429e8b
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-10-25 21:13:32 +0100

    Update deno to v2.0.3
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run -A
+#!/usr/bin/env -S deno run -A --cached-only
 
 import { build, type Plugin, stop } from "esbuild";
 import { denoPlugins } from "@luca/esbuild-deno-loader";
