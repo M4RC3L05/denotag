01cb794fa9e41da979df6b9b43d54704f0e5df94
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2025-09-10 12:23:06 +0100

    Update to deno 2.5.0
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S deno run -A
 
 import $ from "@david/dax";
-import meta from "./../deno.json" with { type: "json" };
 
 $.setPrintCommand(true);
 
@@ -16,36 +15,6 @@ const targets = [
   "x86_64-unknown-linux-gnu",
   "aarch64-unknown-linux-gnu",
 ] as const;
-
-const requiredDepsInfo = await $`deno info --json src/main.ts`.json();
-const requiredDeps = new Set(
-  (requiredDepsInfo.modules as {
-    specifier?: string;
-    kind?: string;
-    dependencies?: { specifier: string }[];
-  }[]).filter((item) =>
-    item.kind === "esm" && item.specifier && URL.canParse(item.specifier) &&
-    item.specifier.includes("denotag/src/") &&
-    Array.isArray(item.dependencies) && item.dependencies.length > 0
-  ).flatMap((item) =>
-    item.dependencies?.filter((dep) => (dep.specifier in meta.imports)).map(
-      (dep) => dep.specifier,
-    )
-  ),
-);
-
-const compileDenoJson = JSON.stringify(
-  {
-    ...meta,
-    imports: Object.fromEntries(
-      requiredDeps.values().map((
-        v,
-      ) => [v, meta.imports[v as keyof typeof meta.imports]]),
-    ),
-  },
-  null,
-  2,
-);
 
 const buildFor = async (target: typeof targets[number]) => {
   const compileDirName = `${binName}-${target}`;
@@ -64,10 +33,10 @@ const buildFor = async (target: typeof targets[number]) => {
     `${compressedBinName}.sha256`,
   );
 
-  await $`deno compile --unstable-raw-imports --cached-only --no-check --allow-env=ENV --allow-net=127.0.0.1 --env=${
+  await $`deno compile --cached-only --no-check --allow-env=ENV --allow-net=127.0.0.1 --env=${
     rootDir.resolve(".env")
   } --target=${target} --output=${compiledPath} ${
-    rootDir.resolve("src", "main.ts")
+    rootDir.resolve("dist", "app.bundle.js")
   }`;
 
   if (target.includes("windows")) {
@@ -83,9 +52,9 @@ const buildFor = async (target: typeof targets[number]) => {
 
 await binDir.emptyDir();
 await $`echo "ENV=production" > ${rootDir.resolve(".env")}`;
+await $`deno install --frozen=true`;
 await $`deno task build`;
-await $`echo ${compileDenoJson} > deno.json`;
-await $`rm -rf node_modules`;
-await $`deno install`;
+await $`deno bundle --output ${rootDir.resolve("dist", "app.bundle.js")} ${
+  rootDir.resolve("src", "main.ts")
+}`;
 await Promise.all(targets.map(buildFor));
-await $`echo ${JSON.stringify(meta, null, 2)} > deno.json`;
